49fa94d4b2e4ffc8d126c8f05f4db134a49ad211
bananajoe33 <adriddfc@gmail.com>
2025-10-08 10:19:11 -0500

    cambios

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/infraestructure/web/rest/ProductoController.java
@@ -0,0 +1,10 @@
+package es.etg.daw.dawes.java.rest.restfull.productos.infraestructure.web.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+	
+}
